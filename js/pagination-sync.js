f2481f8ab1e03a96108c827deefd49aca4a659fd
linxOD <linxOD@users.noreply.github.com>
2022-01-28 10:11:30 +0000

    deploy: de6429852a6e412b1abd0b2f02997ca0abee1c7a

--- js/pagination-sync.js
+++ js/pagination-sync.js
@@ -1,16 +1,15 @@
 $('.pagination .nav-tabs li a').click(function (e) {     
     //get selected href
     var href = $(this).attr('href');
-    var tabCat = href.split('-')[0].replace('#', '');
-    console.log(tabCat);   
+    var dataTab = href.split('-')[0].replace('#', '');
 
     //set all nav tabs to inactive
-    $('.pagination .nav-tabs li a[data-tab="'+tabCat+'"]').removeClass('active');
+    $('.pagination .nav-tabs li a[data-tab="'+dataTab+'"]').removeClass('active');
 
     //get all nav tabs matching the href and set to active
     $('.pagination .nav-tabs li a[href="'+href+'"]').addClass('active');
 
     //active tab
-    $('.pagination-tab.tab-pane[data-tab="'+tabCat+'"]').removeClass('active');   
+    $('.pagination-tab.tab-pane[data-tab="'+dataTab+'"]').removeClass('active');   
     $('.pagination-tab.tab-pane'+href).addClass('active show');
 })
