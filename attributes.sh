8252d5c010fc32be7829664f3aafb680a1a6b220
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-11-28 12:32:42 +0100

    updating attributs script: dev app url, adding memoirs

--- attributes.sh
+++ attributes.sh
@@ -1,6 +1,7 @@
 # bin/bash
 
 echo "add attributes"
-add-attributes -g "./data/editions/correspondence/*.xml" -b "https://auden-musulin-papers.github.io/amp-app"
-add-attributes -g "./data/editions/additional-materials/*.xml" -b "https://auden-musulin-papers.github.io/amp-app"
-add-attributes -g "./data/editions/photos/*.xml" -b "https://auden-musulin-papers.github.io/amp-app"
+add-attributes -g "./data/editions/correspondence/*.xml" -b "https://auden-musulin-papers.github.io/amp-app-ev"
+add-attributes -g "./data/editions/additional-materials/*.xml" -b "https://auden-musulin-papers.github.io/amp-app-dev"
+add-attributes -g "./data/editions/photos/*.xml" -b "https://auden-musulin-papers.github.io/amp-app-dev"
+add-attributes -g "./data/editions/memoirs/*.xml" -b "https://auden-musulin-papers.github.io/amp-app-dev"
