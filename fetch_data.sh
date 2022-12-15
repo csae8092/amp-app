796458a2d57973bd869efa1da53c84acf17d9cde
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-12-15 16:18:23 +0100

    minro chg

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,21 +1,11 @@
 # bin/bash
 
-# rm main.zip
-# rm -rf ./data
-# wget https://github.com/Auden-Musulin-Papers/amp-data/archive/refs/heads/main.zip
-# unzip main
-# mv amp-data-main/data ./data
-# rm -rf amp-data-main
-# rm main.zip
-
-# rm -rf ./data/tmp
-
-rm entities.zip
+rm main.zip
 rm -rf ./data
-wget https://github.com/Auden-Musulin-Papers/amp-data/archive/refs/heads/entities.zip
-unzip entities
-mv amp-data-entities/data ./data
-rm -rf amp-data-entities
-rm entities.zip
+wget https://github.com/Auden-Musulin-Papers/amp-data/archive/refs/heads/main.zip
+unzip main
+mv amp-data-main/data ./data
+rm -rf amp-data-main
+rm main.zip
 
 rm -rf ./data/tmp
