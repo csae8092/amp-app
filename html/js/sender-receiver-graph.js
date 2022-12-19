693630b7febe28adfb3e79c746c398be946f930b
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-12-19 10:17:02 +0100

    minor linter update

--- html/js/sender-receiver-graph.js
+++ html/js/sender-receiver-graph.js
@@ -1,174 +1,169 @@
 Highcharts.getJSON(
-    'js/json/analytics/Sender_receiver_addresses_XY.json',
-    function (data) {
+  "js/json/analytics/Sender_receiver_addresses_XY.json",
+  function (data) {
+    let features = data.features;
+    var auden = [];
+    var audenD = {};
+    var stella = [];
+    var stellaD = {};
+    var mendelson = [];
+    var mendelsonD = {};
+    var sullivan = [];
+    var sullivanD = {};
+    var kraus = [];
+    var krausD = {};
 
-        let features = data.features;
-        var auden = [];
-        var audenD = {};
-        var stella = [];
-        var stellaD = {};
-        var mendelson = [];
-        var mendelsonD = {};
-        var sullivan = [];
-        var sullivanD = {};
-        var kraus = [];
-        var krausD = {};
+    for (let x in features) {
+      if (features[x].attributes.Role == "Sender") {
+        // convert unix date to m/d/y
+        let d = features[x].attributes.Date;
+        let nd = new Date(d);
+        // let ndd = nd.getDate(nd);
+        // let ndm = nd.getMonth(nd);
+        let ndy = nd.getFullYear(nd);
+        // let dr = `${ndd}/${ndm}/${ndy}`;
+        let dr = ndy;
 
-        for (let x in features) {
-
-            if (features[x].attributes.Role == "Sender") {
-                // convert unix date to m/d/y
-                let d = features[x].attributes.Date;
-                let nd = new Date(d);
-                let ndd = nd.getDate(nd);
-                let ndm = nd.getMonth(nd);
-                let ndy = nd.getFullYear(nd);
-                // let dr = `${ndd}/${ndm}/${ndy}`;
-                let dr = ndy;
-
-                if (features[x].attributes.From_Person == "W. H. Auden") {
-                    if (!audenD[dr]) {
-                        audenD[dr] = 0;
-                    }
-                    audenD[dr] += 1;
-                }
-
-                if (features[x].attributes.From_Person == "Stella Musulin") {
-                    if (!stellaD[dr]) {
-                        stellaD[dr] = 0;
-                    }
-                    stellaD[dr] += 1;
-                }
-
-                if (features[x].attributes.From_Person == "Edward Mendelson") {
-                    if (!mendelsonD[dr]) {
-                        mendelsonD[dr] = 0;
-                    }
-                    mendelsonD[dr] += 1;
-                }
-
-                if (features[x].attributes.From_Person == "Michael O'Sullivan") {
-                    if (!sullivanD[dr]) {
-                        sullivanD[dr] = 0;
-                    }
-                    sullivanD[dr] += 1;
-                }
-
-                if (features[x].attributes.From_Person == "Dr. Wolfgang Kraus") {
-                    if (!krausD[dr]) {
-                        krausD[dr] = 0;
-                    }
-                    krausD[dr] += 1;
-                }
-
-            }
+        if (features[x].attributes.From_Person == "W. H. Auden") {
+          if (!audenD[dr]) {
+            audenD[dr] = 0;
+          }
+          audenD[dr] += 1;
         }
 
-        for (let [key, value] of Object.entries(audenD)) {
-            auden.push([Date.UTC(key), value]);
+        if (features[x].attributes.From_Person == "Stella Musulin") {
+          if (!stellaD[dr]) {
+            stellaD[dr] = 0;
+          }
+          stellaD[dr] += 1;
         }
 
-        for (let [key, value] of Object.entries(stellaD)) {
-            stella.push([Date.UTC(key), value]);
+        if (features[x].attributes.From_Person == "Edward Mendelson") {
+          if (!mendelsonD[dr]) {
+            mendelsonD[dr] = 0;
+          }
+          mendelsonD[dr] += 1;
         }
 
-        for (let [key, value] of Object.entries(mendelsonD)) {
-            mendelson.push([Date.UTC(key), value]);
+        if (features[x].attributes.From_Person == "Michael O'Sullivan") {
+          if (!sullivanD[dr]) {
+            sullivanD[dr] = 0;
+          }
+          sullivanD[dr] += 1;
         }
 
-        for (let [key, value] of Object.entries(sullivanD)) {
-            sullivan.push([Date.UTC(key), value]);
+        if (features[x].attributes.From_Person == "Dr. Wolfgang Kraus") {
+          if (!krausD[dr]) {
+            krausD[dr] = 0;
+          }
+          krausD[dr] += 1;
         }
-
-        for (let [key, value] of Object.entries(krausD)) {
-            kraus.push([Date.UTC(key), value]);
-        }
-    
-        Highcharts.chart('container', {
-
-            title: {
-                text: 'Auden Musulin Papers: Correspondence'
-            },
-        
-            // subtitle: {
-            //     text: 'Source" target="_blank">IREC</a>'
-            // },
-        
-            yAxis: {
-                title: {
-                    text: 'Number of Letters'
-                },
-                min: 0
-            },
-        
-            xAxis: {
-                type: 'datetime',
-                // dateTimeLabelFormats: { // don't display the year
-                //     month: '%e. %b',
-                //     year: '%b'
-                // },
-                title: {
-                    text: 'Year'
-                }
-            },
-        
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'middle'
-            },
-        
-            plotOptions: {
-                series: {
-                    marker: {
-                        enabled: true,
-                        radius: 2.5
-                    }
-                }
-            },
-
-            series: [
-                {
-                    name: 'Auden W.H.',
-                    // lineWidth: 4,
-                    // marker: {
-                    //     radius: 4
-                    // },
-                    data: auden
-                }, 
-                {
-                    name: 'Stella Musulin',
-                    data: stella
-                },
-                {
-                    name: 'Edward Mendelson',
-                    data: mendelson
-                },
-                {
-                    name: 'Michael O`Sullivan',
-                    data: sullivan
-                },
-                {
-                    name: 'Dr. Wolfgang Kraus',
-                    data: kraus
-                }
-            ]
-        
-            // responsive: {
-            //     rules: [{
-            //         condition: {
-            //             maxWidth: 500
-            //         },
-            //         chartOptions: {
-            //             legend: {
-            //                 layout: 'horizontal',
-            //                 align: 'center',
-            //                 verticalAlign: 'bottom'
-            //             }
-            //         }
-            //     }]
-            // }
-        
-        });
+      }
     }
+
+    for (let [key, value] of Object.entries(audenD)) {
+      auden.push([Date.UTC(key), value]);
+    }
+
+    for (let [key, value] of Object.entries(stellaD)) {
+      stella.push([Date.UTC(key), value]);
+    }
+
+    for (let [key, value] of Object.entries(mendelsonD)) {
+      mendelson.push([Date.UTC(key), value]);
+    }
+
+    for (let [key, value] of Object.entries(sullivanD)) {
+      sullivan.push([Date.UTC(key), value]);
+    }
+
+    for (let [key, value] of Object.entries(krausD)) {
+      kraus.push([Date.UTC(key), value]);
+    }
+
+    Highcharts.chart("container", {
+      title: {
+        text: "Auden Musulin Papers: Correspondence",
+      },
+
+      // subtitle: {
+      //     text: 'Source" target="_blank">IREC</a>'
+      // },
+
+      yAxis: {
+        title: {
+          text: "Number of Letters",
+        },
+        min: 0,
+      },
+
+      xAxis: {
+        type: "datetime",
+        // dateTimeLabelFormats: { // don't display the year
+        //     month: '%e. %b',
+        //     year: '%b'
+        // },
+        title: {
+          text: "Year",
+        },
+      },
+
+      legend: {
+        layout: "vertical",
+        align: "right",
+        verticalAlign: "middle",
+      },
+
+      plotOptions: {
+        series: {
+          marker: {
+            enabled: true,
+            radius: 2.5,
+          },
+        },
+      },
+
+      series: [
+        {
+          name: "Auden W.H.",
+          // lineWidth: 4,
+          // marker: {
+          //     radius: 4
+          // },
+          data: auden,
+        },
+        {
+          name: "Stella Musulin",
+          data: stella,
+        },
+        {
+          name: "Edward Mendelson",
+          data: mendelson,
+        },
+        {
+          name: "Michael O`Sullivan",
+          data: sullivan,
+        },
+        {
+          name: "Dr. Wolfgang Kraus",
+          data: kraus,
+        },
+      ],
+
+      // responsive: {
+      //     rules: [{
+      //         condition: {
+      //             maxWidth: 500
+      //         },
+      //         chartOptions: {
+      //             legend: {
+      //                 layout: 'horizontal',
+      //                 align: 'center',
+      //                 verticalAlign: 'bottom'
+      //             }
+      //         }
+      //     }]
+      // }
+    });
+  }
 );
