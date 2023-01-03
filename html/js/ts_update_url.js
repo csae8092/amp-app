08b622d75269dcb5ccc02e06b963c0f3341459e9
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2023-01-03 14:54:14 +0100

    testing ts url update refinements

--- html/js/ts_update_url.js
+++ html/js/ts_update_url.js
@@ -1,9 +1,33 @@
 var tsInput = document.querySelector("input[type='search']");
+tsInput.addEventListener("input", updateHeaderUrl);
+
+// setTimeout(() => {
+//   var tsRefInput = document.querySelectorAll(".ais-RefinementList-checkbox");
+//   console.log(typeof tsRefInput);
+//   tsRefInput.forEach((el) => {
+//     console.log(el);
+//     console.log(typeof el);
+//     el.addEventListener("input", updateHeaderUrl);
+//   });
+// }, 50);
 
 function updateHeaderUrl() {
   setTimeout(() => {
+    // var tsRefinements = document.querySelectorAll(
+    //   ".ais-CurrentRefinements-categoryLabel"
+    // );
+
+    // if (tsRefinements) {
+    //   refinements = "";
+    //   tsRefinements.forEach((el) => {
+    //     console.log(el.innerHTML);
+    //     refinements += `+${el.innerHTML}`;
+    //   });
+    // }
+
     var urlToUpdate = document.querySelectorAll(".ais-Hits-item h4 a");
     var tsInputVal = tsInput.value;
+
     urlToUpdate.forEach((el) => {
       var urlToUpdateHref = el.getAttribute("href");
       if (urlToUpdateHref.includes("&mark")) {
@@ -19,5 +43,3 @@ function updateHeaderUrl() {
     });
   }, 500);
 }
-
-tsInput.addEventListener("input", updateHeaderUrl);
