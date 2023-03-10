3d71dd43547409b5a5119ab0f59a94ac89a2bc29
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2023-03-10 11:12:14 +0100

    minor fix: verify if prev or next exists

--- html/js/prev-next-urlupdate.js
+++ html/js/prev-next-urlupdate.js
@@ -1,3 +1,5 @@
+window.onload = nextPrevUrl();
+
 var features1 = document.getElementsByClassName("features-1");
 var features2 = document.getElementsByClassName("features-2");
 
@@ -8,8 +10,6 @@ var features2 = document.getElementsByClassName("features-2");
 [].forEach.call(features2, (opt) => {
   opt.addEventListener("click", nextPrevUrlUpdate);
 });
-
-window.onload = nextPrevUrl();
 
 function nextPrevUrl() {
   var prev = document.getElementById("prev-doc");
@@ -23,32 +23,42 @@ function nextPrevUrl() {
   } else {
     path = "";
   }
-  var prev_href = prev.getAttribute("href");
-  var next_href = next.getAttribute("href");
-  var new_prev = new URL(`${domain}/${path}/${prev_href}?${urlparam}`);
-  var new_next = new URL(`${domain}/${path}/${next_href}?${urlparam}`);
-  prev.setAttribute("href", new_prev);
-  next.setAttribute("href", new_next);
+  if (prev) {
+    var prev_href = prev.getAttribute("href");
+    var new_prev = new URL(`${domain}/${path}/${prev_href}?${urlparam}`);
+    prev.setAttribute("href", new_prev);
+  }
+  if (next) {
+    var next_href = next.getAttribute("href");
+    var new_next = new URL(`${domain}/${path}/${next_href}?${urlparam}`);
+    next.setAttribute("href", new_next);
+  }
 }
 
 function nextPrevUrlUpdate() {
   var prev = document.getElementById("prev-doc");
   var next = document.getElementById("next-doc");
   var urlparam = new URLSearchParams(document.location.search);
-  var prev_href = new URL(prev.getAttribute("href"));
-  var next_href = new URL(next.getAttribute("href"));
-  var old_prev_search = new URLSearchParams(prev_href.search);
-  var old_next_search = new URLSearchParams(next_href.search);
-  urlparam.forEach((value, key) => {
-    old_prev_search.set(key, value);
-    old_next_search.set(key, value);
-  });
-  prev.setAttribute(
-    "href",
-    `${prev_href.origin}${prev_href.pathname}?${old_prev_search.toString()}`
-  );
-  next.setAttribute(
-    "href",
-    `${next_href.origin}${next_href.pathname}?${old_next_search.toString()}`
-  );
+  if (prev) {
+    var prev_href = new URL(prev.getAttribute("href"));
+    var old_prev_search = new URLSearchParams(prev_href.search);
+    urlparam.forEach((value, key) => {
+      old_prev_search.set(key, value);
+    });
+    prev.setAttribute(
+      "href",
+      `${prev_href.origin}${prev_href.pathname}?${old_prev_search.toString()}`
+    );
+  }
+  if (next) {
+    var next_href = new URL(next.getAttribute("href"));
+    var old_next_search = new URLSearchParams(next_href.search);
+    urlparam.forEach((value, key) => {
+      old_next_search.set(key, value);
+    });
+    next.setAttribute(
+      "href",
+      `${next_href.origin}${next_href.pathname}?${old_next_search.toString()}`
+    );
+  }
 }
