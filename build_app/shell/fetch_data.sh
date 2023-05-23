2bbef8098c4f8a725e9a9150c2a9411ba5553907
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2023-05-23 13:16:42 +0200

    switched to main

--- build_app/shell/fetch_data.sh
+++ build_app/shell/fetch_data.sh
@@ -1,21 +1,21 @@
 # bin/bash
 
-# rm main.zip
-# rm -rf ./data
-# wget https://github.com/Auden-Musulin-Papers/amp-data/archive/refs/heads/main.zip
-# unzip main
-# mv amp-data-main/data ./data
-# rm -rf amp-data-main
-# rm main.zip
-
-# rm -rf ./data/tmp
-
-rm dev.zip
+rm main.zip
 rm -rf ./data
-wget https://github.com/Auden-Musulin-Papers/amp-data/archive/refs/heads/dev.zip
-unzip dev
-mv amp-data-dev/data ./data
-rm -rf amp-data-dev
-rm dev.zip
+wget https://github.com/Auden-Musulin-Papers/amp-data/archive/refs/heads/main.zip
+unzip main
+mv amp-data-main/data ./data
+rm -rf amp-data-main
+rm main.zip
 
 rm -rf ./data/tmp
+
+# rm dev.zip
+# rm -rf ./data
+# wget https://github.com/Auden-Musulin-Papers/amp-data/archive/refs/heads/dev.zip
+# unzip dev
+# mv amp-data-dev/data ./data
+# rm -rf amp-data-dev
+# rm dev.zip
+
+# rm -rf ./data/tmp
