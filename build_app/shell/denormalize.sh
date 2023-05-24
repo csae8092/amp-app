750755c8b42dbab4577eddcfbb0f814749e1a822
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2023-05-24 09:44:21 +0200

    minor fix author

--- build_app/shell/denormalize.sh
+++ build_app/shell/denormalize.sh
@@ -1,3 +1,3 @@
 # bin/bash
 
-denormalize-indices -f "./data/editions/*/*.xml" -i "./data/indices/*.xml" -m ".//tei:rs[@ref]/@ref" -x ".//tei:title[@level='a']/text()" -xs ".//tei:author/text()" -d ".//tei:origDate/@notBefore"
+denormalize-indices -f "./data/editions/*/*.xml" -i "./data/indices/*.xml" -m ".//tei:rs[@ref]/@ref" -x ".//tei:title[@level='a']/text()" -xs ".//tei:titleStmt/tei:author/text()" -d ".//tei:origDate/@notBefore"
