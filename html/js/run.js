522bcccabb2658a0a5ce558e7e18172c81491ee8
Daniel_Stoxreiter <elsner.daniel@outlook.com>
2023-12-05 13:11:47 +0100

    #75

--- html/js/run.js
+++ html/js/run.js
@@ -230,14 +230,14 @@ var editor = new LoadEditor({
       },
     ],
     active_class: "active",
-    html_class: "custom-select",
+    html_class: "font-dropdown",
   },
   fs: {
     name: "Create full size mode",
     variants: [
       {
         opt: "ef",
-        title: "Full screen on/off",
+        title: "Fullscreen",
         urlparam: "fs",
         chg_citation: "citation-url",
         hide: "hide-reading",
@@ -245,7 +245,7 @@ var editor = new LoadEditor({
       },
     ],
     active_class: "active",
-    render_class: "nav-link btn btn-round",
+    render_class: "window-screen-btn",
     render_svg:
       "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-fullscreen' viewBox='0 0 16 16'><path d='M1.5 1a.5.5 0 0 0-.5.5v4a.5.5 0 0 1-1 0v-4A1.5 1.5 0 0 1 1.5 0h4a.5.5 0 0 1 0 1h-4zM10 .5a.5.5 0 0 1 .5-.5h4A1.5 1.5 0 0 1 16 1.5v4a.5.5 0 0 1-1 0v-4a.5.5 0 0 0-.5-.5h-4a.5.5 0 0 1-.5-.5zM.5 10a.5.5 0 0 1 .5.5v4a.5.5 0 0 0 .5.5h4a.5.5 0 0 1 0 1h-4A1.5 1.5 0 0 1 0 14.5v-4a.5.5 0 0 1 .5-.5zm15 0a.5.5 0 0 1 .5.5v4a1.5 1.5 0 0 1-1.5 1.5h-4a.5.5 0 0 1 0-1h4a.5.5 0 0 0 .5-.5v-4a.5.5 0 0 1 .5-.5z'/></svg>",
   },
@@ -270,14 +270,14 @@ var editor = new LoadEditor({
       },
     ],
     active_class: "active",
-    html_class: "custom-select",
+    html_class: "font-dropdown",
   },
   is: {
     name: "enable/disable image viewer",
     variants: [
       {
         opt: "es",
-        title: "on/off",
+        title: "Image",
         urlparam: "img",
         chg_citation: "citation-url",
         fade: "fade",
@@ -301,7 +301,7 @@ var editor = new LoadEditor({
     ],
     active_class: "active",
     rendered_element: {
-      a_class: "nav-link btn btn-round",
+      a_class: "img-viewer-btn",
       svg: "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-image' viewBox='0 0 16 16'><path d='M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z'/><path d='M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h-12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z'/></svg>",
     },
   },
