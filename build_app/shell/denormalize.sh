8b8fbe6edd3e47b9c464dddd21ca9e11beb20013
Daniel_Stoxreiter <elsner.daniel@outlook.com>
2024-02-20 14:23:31 +0100

    fixing interp titles in document title #153

--- build_app/shell/denormalize.sh
+++ build_app/shell/denormalize.sh
@@ -1,3 +1,3 @@
 # bin/bash
 
-denormalize-indices -f "./data/editions/*/*.xml" -i "./data/indices/*.xml" -m ".//tei:rs[@ref]/@ref|//tei:quote[@source]/@source|.//tei:bibl[@sameAs]/@sameAs" -x ".//tei:title[@level='a']/text()"
+denormalize-indices -f "./data/editions/*/*.xml" -i "./data/indices/*.xml" -m ".//tei:rs[@ref]/@ref|//tei:quote[@source]/@source" -x ".//tei:titleStmt/tei:title[@level='a']/text()"
